5340fa298a2146cbe8f91553cfd6ed30b18a17ff
Dominos0321 <dsch792@gmail.com>
2022-12-16 17:06:31 +0100

    gotowe

--- app/src/main/java/com/ft_ss/sotrowania/MainActivity.kt
+++ app/src/main/java/com/ft_ss/sotrowania/MainActivity.kt
@@ -2,39 +2,72 @@ package com.ft_ss.sotrowania
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.coroutines.flow.merge
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        var lbl = findViewById<TextView>(R.id.wynlbl)
-        var lbl2 = findViewById<TextView>(R.id.srtlbl)
         var tab : IntArray = intArrayOf()
+        var heapSize = 0
 
         fun removeLastItem(array: IntArray): IntArray {
             return array.copyOf(array.lastIndex)
         }
 
         fun czysc() {
-            lbl.text = ""
-            lbl2.text = ""
-
             for (i in 0..tab.size - 1) {
                 tab = removeLastItem(tab)
             }
         }
 
-        fun wypisz() {
-            for (i in 0 until (tab.size)) {
-                var text = lbl2.text
-                lbl2.text = text.toString() + "  " + tab[i].toString()
+
+
+        //          SORTOWANIE SZYBKIE / QUICKSORT
+
+        fun swapArray(a: IntArray, b: Int, c: Int) {
+            val temp = a[b]
+            a[b] = a[c]
+            a[c] = temp
+        }
+        fun partition(array: IntArray, l: Int, r: Int): Int {
+            var left = l
+            var right = r
+            val pivot = array[(left + right)/2] // 4) Pivot Point
+            while (left <= right) {
+                while (array[left] < pivot) left++ // 5) Find the elements on left that should be on right
+
+                while (array[right] > pivot) right-- // 6) Find the elements on right that should be on left
+
+                // 7) Swap elements, and move left and right indices
+                if (left <= right) {
+                    swapArray(array, left,right)
+                    left++
+                    right--
+                }
+            }
+            return left
+        }
+        fun quickSort(array: IntArray, left: Int, right: Int) {
+            val index = partition(array, left, right)
+            if(left < index-1) { // 2) Sorting left half
+                quickSort(array, left, index-1)
+            }
+            if(index < right) { // 3) Sorting right half
+                quickSort(array,index, right)
             }
         }
+
+
+
+
+
+        //          SORTOWANIE BĄBELKOWE
 
         fun sortbb(tab : IntArray) { //działą
             for (i in 0 until (tab.size - 1)) {
@@ -47,6 +80,10 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+
+
+        //          SORTOWANIE PRZEZ WSTAWIANIE
 
         fun wsta(tab: IntArray) { //działa
             for (count in 1..tab.count() - 1){
@@ -62,59 +99,146 @@ class MainActivity : AppCompatActivity() {
 
 
 
+        //          HEAPSORT
 
+        fun swap(A: IntArray, i: Int, j: Int) {
+            var temp = A[i]
+            A[i] = A[j]
+            A[j] = temp
+        }
 
+        fun max_heapify(A: IntArray, i: Int) {
+            var l = 2*i;
+            var r = 2*i+1;
+            var largest: Int;
 
-        fun mergeOptimized(numArr: IntArray, start: Int, mid: Int, end: Int) {
-            val leftArray = numArr.copyOfRange(start, mid)
-            val rightArray = numArr.copyOfRange(mid, end)
-            var i=0
-            var j=0
-            for(k in start until end) {
-                if((j>=rightArray.size) || (i<leftArray.size && leftArray[i]<=rightArray[j])) {
-                    numArr[k]=leftArray[i]
-                    i++
-                } else {
-                    numArr[k]=rightArray[j]
-                    j++
-                }
+            if ((l <= heapSize - 1) && (A[l] > A[i])) {
+                largest = l;
+            } else
+                largest = i
+
+            if ((r <= heapSize - 1) && (A[r] > A[l])) {
+                largest = r
+            }
+
+            if (largest != i) {
+                swap(A, i, largest);
+                max_heapify(A, largest);
             }
         }
 
-        fun mergeSortInternalOptimized(numArr: IntArray, start: Int, end: Int) {
-            if(start>=end)
+        fun heap_sort(A: IntArray) {
+            heapSize = A.size
+            for (i in heapSize / 2 downTo 0) {
+                max_heapify(A, i)
+            }
+            for (i in A.size - 1 downTo 1) {
+                swap(A, i, 0)
+                heapSize = heapSize - 1
+                max_heapify(A, 0)
+
+            }
+        }
+
+
+
+        //          SORTOWANIE PRZEZ SCALANIE / MERGESORT
+
+        fun mergeHalvesIA(workA: IntArray,
+                                          workB: IntArray,
+                                          start: Int,
+                                          mid: Int,
+                                          exclusiveEnd: Int) {
+            var p1 = start
+            var p2 = mid
+            for (i in start until exclusiveEnd) {
+                if (p1 < mid && (p2 == exclusiveEnd || workA[p1] <= workA[p2])) {
+                    workB[i] = workA[p1]
+                    p1++
+                } else {
+                    workB[i] = workA[p2]
+                    p2++
+                }
+            }
+        }
+        fun sortSectionIA(input: IntArray,
+                                   output: IntArray,
+                                   start: Int,
+                                   exclusiveEnd: Int) : Unit {
+
+            if (exclusiveEnd - start <= 1)
                 return
-            val mid = (start+end)/2
-            mergeSortInternalOptimized(numArr,start, mid)
-            mergeSortInternalOptimized(numArr,mid+1,end)
-            mergeOptimized(numArr,start,mid,end)
+            val mid = (start + exclusiveEnd) / 2
+            sortSectionIA(output, input, start, mid)
+            sortSectionIA(output, input, mid, exclusiveEnd)
+            mergeHalvesIA(input, output, start, mid, exclusiveEnd)
+        }
+        fun mergeSortIA(array: IntArray) : IntArray {
+            val arrayCopy = array.copyOf()
+            val arrayCopy2 = array.copyOf()
+
+            sortSectionIA(arrayCopy, arrayCopy2, 0, arrayCopy.size)
+
+            return arrayCopy2
         }
 
-        fun mergeSortOptimized(numArr: IntArray) {
-            mergeSortInternalOptimized(numArr,0,numArr.size)
-            wypisz()
-        }
 
 
 
 
-        fun losuj(ile: Int) {
+        fun losuj(ile: Int, ktore: Int) {
             czysc()
             for (i in 0..ile - 1) {
                 tab += (1..9).random()
             }
 
-            for (i in 0 until (tab.size)) {
-                var text = lbl.text
-                lbl.text = text.toString() + "  " + tab[i].toString()
-            }
+            var a: MutableList<Int> = mutableListOf()
 
-            mergeSortOptimized(tab)
+
+            if(ktore == 1)
+                quickSort(tab, 0, tab.size - 1)
+            if(ktore == 2)
+                wsta(tab)
+            if(ktore == 3)
+                sortbb(tab)
+            if(ktore == 4)
+                heap_sort(tab)
+            else
+                mergeSortIA(tab)
+
+
         }
 
-
         findViewById<Button>(R.id.button).setOnClickListener {
-            losuj(findViewById<EditText>(R.id.ilee).text.toString().toInt())
+            var quickStart = System.currentTimeMillis()
+            for(i in 0..findViewById<EditText>(R.id.iler).text.toString().toInt())
+                losuj(findViewById<EditText>(R.id.ilee).text.toString().toInt(), 1)
+            var quickTime = System.currentTimeMillis() - quickStart;
+            findViewById<TextView>(R.id.quicklbl).text = quickTime.toString() + " milisekund"
+
+            var wstStart = System.currentTimeMillis()
+            for(i in 0..findViewById<EditText>(R.id.iler).text.toString().toInt())
+                losuj(findViewById<EditText>(R.id.ilee).text.toString().toInt(), 2)
+            var wstTime = System.currentTimeMillis() - wstStart;
+            findViewById<TextView>(R.id.wstlbl).text = wstTime.toString() + " milisekund"
+
+            var bbStart = System.currentTimeMillis()
+            for(i in 0..findViewById<EditText>(R.id.iler).text.toString().toInt())
+                losuj(findViewById<EditText>(R.id.ilee).text.toString().toInt(), 3)
+            var bbTime = System.currentTimeMillis() - bbStart;
+            findViewById<TextView>(R.id.bablbl).text = bbTime.toString() + " milisekund"
+
+            var heapStart = System.currentTimeMillis()
+            for(i in 0..findViewById<EditText>(R.id.iler).text.toString().toInt())
+                losuj(findViewById<EditText>(R.id.ilee).text.toString().toInt(), 4)
+            var heapTime = System.currentTimeMillis() - heapStart;
+            findViewById<TextView>(R.id.heplbl).text = heapTime.toString() + " milisekund"
+
+            var mergeStart = System.currentTimeMillis()
+            for(i in 0..findViewById<EditText>(R.id.iler).text.toString().toInt())
+                losuj(findViewById<EditText>(R.id.ilee).text.toString().toInt(), 5)
+            var mergeTime = System.currentTimeMillis() - mergeStart;
+            findViewById<TextView>(R.id.scalbl).text = mergeTime.toString() + " milisekund"
         }
     }
 }
